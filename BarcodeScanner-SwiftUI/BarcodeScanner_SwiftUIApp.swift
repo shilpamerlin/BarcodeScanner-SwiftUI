bf7a6c36f8e61f147daba509f234ebe1598dc1e4
Shilpa Merlin joy <shilpamjoy@gmail.com>
2024-07-02 14:07:35 -0400

    Initial Commit

---
+++ BarcodeScanner-SwiftUI/BarcodeScanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner_SwiftUIApp.swift
+//  BarcodeScanner-SwiftUI
+//
+//  Created by Shilpa Joy on 2024-07-02.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
